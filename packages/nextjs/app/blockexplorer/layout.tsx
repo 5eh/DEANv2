3d9b22e973c3347996f390a2043d16e3bde36d60
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2024-06-28 09:25:24 +0700

    :zap: Removed MIT License :zap:

--- packages/nextjs/app/blockexplorer/layout.tsx
+++ packages/nextjs/app/blockexplorer/layout.tsx
@@ -1,8 +1,9 @@
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+import { COMPANY_DESCRIPTION, COMPANY_NAME } from "../../../../configuration/company";
 
 export const metadata = getMetadata({
-  title: "Block Explorer",
-  description: "Block Explorer created with 🏗 Scaffold-ETH 2",
+  title: `Transactions | ${COMPANY_NAME}`,
+  description: `${COMPANY_DESCRIPTION}`,
 });
 
 const BlockExplorerLayout = ({ children }: { children: React.ReactNode }) => {
