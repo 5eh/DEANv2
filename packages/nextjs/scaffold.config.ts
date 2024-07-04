e1c521c85e2e10f94a5c40e92051d1a1820ba596
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2024-07-04 15:15:29 +0700

    :zap: Added MEVM to localchain. This now is functional, but needing to now test contract :zap:

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,5 +1,31 @@
 import * as chains from "viem/chains";
 
+import { defineChain } from "viem";
+
+export const mevm = defineChain({
+  id: 336,
+  name: "Movement EVM",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Move Tokens",
+    symbol: "MOVE",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://mevm.devnet.m1.movementlabs.xyz"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://explorer.movementlabs.xyz/#/?network=local" },
+  },
+  contracts: {
+    multicall3: {
+      address: "0x4821b8c6B10a12933bF790823eb992cBCb563639",
+      blockCreated: 5882,
+    },
+  },
+});
+
 export type ScaffoldConfig = {
   targetNetworks: readonly chains.Chain[];
   pollingInterval: number;
@@ -10,7 +36,7 @@ export type ScaffoldConfig = {
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.hardhat],
+  targetNetworks: [mevm],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
